77336d7e15a03f836185d7c7fa3c5a8ade44295a
Eugeniy <basove85@mail.ru>
2023-11-18 18:06:38 +0300

    lesson3

--- Script.js
+++ Script.js
@@ -1,12 +1,53 @@
-let lkg = document.getElementById('kg');
-let lfnt = document.getElementById('fnt');
-let lRes = document.getElementById('res');
+// let condition = false;
+// let userName = prompt('Введите свое имя');
+// console.log(userName);
+//switch(userName){
+//    case 'Путин':
+//       alert('Ты внезапно стал Путиным');
+//    break;
+//    case 'Байден':
+//        alert('Ты старина Байден, ты забылся');
+//        break;
+//     case 'Шарик':
+//         alert('Шарик, убери лапу с клавы!');
+//         break;
+//     case 'Барсик':
+//         alert('Ты Барсик');
+//         break;
+//     default:
+//         alert('Ты какое-то непонятное чудо! Определись!');
+// }
 
-function convertKgToFunt(){
-    let kg = lkg.value;
-    let fnt = lfnt.value;
-    const a = 0.454;
-    let result = kg / a;
-    console.log('Результат в фунтах ' + result + ' килограммов');
-    lRes.innerText = result;
+// for(let i = 0; i < 10; i++){
+//     console.log(i*5);
+// }
+
+// while(userName != 'q'){
+//     console.log(userName);
+//     alert('Посмотри на меня');
+//     userName = prompt('Для того, чтобы выйти введите q');
+//     if(userName == 'Dog'){
+//         alert('Продолжаем. Введено "Dog"');
+//         continue;
+//     }
+//     if(userName == 'Cat'){
+//         alert('Вываливаемся из цикла');
+//         break;
+//     }
+// }
+
+// let ar = ['Шарик','Бобик','Барсик','Мурзик','Муська'];
+// console.log(ar.length);
+// ar.push('Иван Иваныч');
+// console.log(ar.length);
+// console.table(ar);
+
+let MoneySumm = document.getElementById('money').value;
+let MoneySelect = document.getElementById('checkMoney').value;
+let outResult = document.getElementById('out');
+let arr = [82, 12, 97, 0.6];
+
+function converter(){
+    let res = MoneySumm * arr[MoneySelect -1];
+    outResult.innerText = `Стоимость по курсу ${res}`;
 }
