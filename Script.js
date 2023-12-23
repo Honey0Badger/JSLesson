dd24e807ba8911e3487c33ed2192f3cbc20562e1
Eugeniy <basove85@mail.ru>
2023-12-23 16:23:45 +0300

    lesson7

--- Script.js
+++ Script.js
@@ -2,21 +2,25 @@ let btn = document.getElementById('btn');
 let temp = document.getElementById('fNode');
 let canv = document.getElementById('canv');
 btn.addEventListener('click', loadPhoto);
-
+let path = 'https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=';
 function loadPhoto(rangePhoto){
-    let result = fetch('https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=10')
-    .then(result1 => result1.json()).then(result2 => result2.photos).then(a => {
-    let arr=[];
-    for(let i=0; i < 10; i++){
-        arr.push(a[i].url); 
-    }
-    return arr;
-    }).then(b => {
-    for(let i =0; i < 10; i++){
-        let photo = temp.content.cloneNode(true);
-        photo.getElementById('img').setAttribute('src', b[i]);
-        console.log(b[i]);
-        canv.appendChild(photo);
-    }
-    });
-};
+    const limit = setNumber();
+    let url = path + limit;
+    fetch(url).then(result1 => result1.json()).then(result2 => result2.photos).then(a => {
+        let arr=[];
+        for(let i=0; i < limit; i++){
+            arr.push(a[i].url);
+        }})
+        return arr;   
+        }.then(b =>{
+            for(let i = 0; i < limit; i++){
+                let photo = temp.content.cloneNode(true);
+                photo.getElementById('ft').setAttribute('src',b[i]);
+                console.log(b[i]);
+                canv.appendChild(photo);
+            }
+        });
+
+function setNumber(){
+    let limit = document.getElementById('photoNumber');
+}
